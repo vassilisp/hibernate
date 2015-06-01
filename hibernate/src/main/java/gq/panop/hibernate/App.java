777afac6af9c3b373a43185ec10b3d208c29a536
Vasileios Panopoulos <vpan@kth.se>
2015-06-01 11:07:57 +0200

    major refactoring - rename of project folder (minor typo)

---
+++ hibernate/src/main/java/gq/panop/hibernate/App.java
@@ -0,0 +1,170 @@
+package gq.panop.hibernate;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.List;
+import java.util.Scanner;
+
+import gq.panop.hibernate.dao.AccessLogDao;
+import gq.panop.hibernate.dao.AuditLogDao;
+import gq.panop.hibernate.model.AccessLog;
+import gq.panop.hibernate.model.AuditLog;
+import gq.panop.util.PerformanceUtil;
+
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+
+    public static void main( String[] args ){
+
+        String userId = "mcico";
+        
+        PerformanceUtil performance = new PerformanceUtil("ms");
+    	AuditLogDao dao = new AuditLogDao();
+        /*
+
+    	for (AuditLog auditLog :  dao.getAllLogs()){
+    		System.out.println(auditLog.toString());
+    	}
+    	*/
+
+    	
+    	performance.Tick();
+    	List<String> userIds = dao.getAllUsers();
+    	performance.Tock();
+    	for (String user : userIds){
+    		System.out.println(user);
+    	}
+    	
+    	
+    	//Create a keyboard scanner and verify userId existance
+    	Boolean foundFlag = false;
+		Scanner keyboard = new Scanner(System.in);
+    	while(!foundFlag){
+    		System.out.print("perform database request for userId: ");
+    		userId = keyboard.next();
+
+    		for (String user : userIds){
+    			if (user.equals(userId)){
+    				foundFlag=true;
+    				break;
+    			}
+    		}
+    		if (foundFlag==true){
+    			System.out.println("Performing Search for userId: " + userId);
+    			break;
+    		}else{System.out.println("UserId not Found in list of available users");}
+    	}
+    	keyboard.close();
+    	
+    	for (AuditLog auditLogs : dao.getAuditLogs(userId)){
+    		System.out.println(auditLogs.toString());
+    	}
+    	
+    	performance.Tick();
+    	for (AuditLog auditLogs : dao.getAuditLogs(userId)){
+    		System.out.println(auditLogs.getTransactionId());
+    	}
+    	performance.Tock("slow AuditLog retrieval by userId took");
+    	separate();
+
+    	
+    	performance.Tick();
+    	List<String> transactionIds = dao.getTransactionIds2(userId);
+    	for (String transactionId : transactionIds){
+    		System.out.println(transactionId);
+    	}
+    	performance.Tock("fast AuditLog retrieval by userId took");
+    	separate();
+
+  
+    	
+    	
+    	AccessLogDao accessLogDao = new AccessLogDao();
+    	
+    	//Retrieve a transactionId by providing a transactionId
+    	performance.Tick();
+    	String transactionId = "c0a80dcd-0dfd-aaccc-146b854ed38-00007c51";
+    	AccessLog accessLog = accessLogDao.getAccessLog(transactionId);
+    	System.out.println(accessLog.toString());
+    	performance.Tock("Retrieve a transactionId by providing a transactionId");
+    	separate();
+    	
+    	//Retrieving all accessLogs by providing a list of transactionIds
+    	performance.Tick();
+    	List<AccessLog> accessLogs = accessLogDao.getAccessLogs(transactionIds);
+    	if(accessLogs.isEmpty()) System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@-- result is empty");
+    	for (AccessLog accLog : accessLogs){
+    		System.out.println(accLog.toString());
+    	}
+    	performance.Tock("Retrieving all accessLogs by providing a list of transactionIds");
+    	separate();
+    	
+    	//Retrieving all accessLogs by providing a userId (all done by MySQL
+    	performance.Tick();
+    	List<AccessLog> accessLogsbyUser = accessLogDao.getAccessLogs(userId);
+    	for (AccessLog accLog: accessLogsbyUser){
+
+    		System.out.println(accLog.toString());
+    		Date realDate = new Date(accLog.getRequestDate().longValue());
+    		System.out.println("The date is : " + realDate);
+    	}
+    	performance.Tock("Retrieving all accessLogs by providing a userId (all done by MySQL)");
+
+    	System.out.println("half half length result size : " + accessLogs.size() + "  , full length result size :  " + accessLogs.size());
+    	
+    	
+    	//create comparator required to sort the result lists based on their timestamps
+    	Comparator<AccessLog> accessLogCompare = new Comparator<AccessLog>(){
+			@Override
+			public int compare(AccessLog aL1, AccessLog aL2){
+				return (aL1.getRequestDate().longValue() > aL2.getRequestDate().longValue())? 1 : -1 ;
+			}	
+    	};
+    	
+    	performance.Tick();
+    	Collections.sort(accessLogsbyUser, accessLogCompare);
+    	Collections.sort(accessLogs, accessLogCompare);
+    	performance.Tock("sorting of result lists");
+
+    	/*
+    	for(int i=0; i< accessLogs.size(); i++)	{
+    		System.out.println(accessLogs.get(i));
+    		System.out.println(accessLogsbyUser.get(i));
+    		System.out.println(accessLogsbyUser.get(i).equals(accessLogs.get(i)));
+    	}
+    	*/
+    	
+    	/*
+		Date realDate = new Date();    	
+    	for (int i=0;i<10;i++){
+    		long time = System.currentTimeMillis();
+        	System.out.println(time);
+
+    		realDate.setTime(time);
+    		System.out.println("The date is : " + realDate + "  |  " + realDate.getHours()+ "  |  " +realDate.getMinutes() + "  |  " + realDate.getSeconds());
+    		try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+    	}
+    	*/
+		
+    	
+    }
+    
+
+    private static void separate(){
+    	System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+    	System.out.println("");
+    }
+    
+}
---
+++ hibernate/src/main/java/gq/panop/hibernate/dao/AccessLogDao.java
@@ -0,0 +1,54 @@
+package gq.panop.hibernate.dao;
+
+import gq.panop.hibernate.model.AccessLog;
+import gq.panop.util.HibernateUtil;
+
+import java.util.List;
+
+import org.hibernate.Query;
+
+public class AccessLogDao {
+
+	public AccessLog getAccessLog(String transactionId){
+		AccessLog accessLog = null;
+		
+		String queryString = "From AccessLog where transactionId = :transactionId";
+		
+    	Query query = HibernateUtil.getSessionFactory().openSession().createQuery(queryString);
+    	query.setString("transactionId", transactionId);
+    	
+    	accessLog = (AccessLog) HibernateUtil.performUniqueQuery(query);
+		return accessLog ;
+	}
+	
+	public List<AccessLog> getAccessLogs(List<String> transactionIds){
+
+		
+		String queryString = "FROM AccessLog WHERE transactionId IN (:transactionIds)";
+		
+		Query query = HibernateUtil.getSessionFactory().openSession().createQuery(queryString);
+		query.setParameterList("transactionIds", transactionIds);
+		
+		List<AccessLog> accessLogs = HibernateUtil.performSimpleQuery(query);
+		return accessLogs;
+	}
+	
+	public List<AccessLog> getAccessLogs(String userId_fromAuditLog){
+		
+		//String queryString = "FROM AccessLog WHERE transactionId IN ( SELECT transactionId FROM AuditLog WHERE userId = :userId )";
+		//old optimized//  String queryString = "Select acl FROM AccessLog acl JOIN acl.auditLog WHERE acl.transactionId=acl.auditLog.transactionId ANDacl.auditLog.userId =:userId";
+		String queryString = "Select acl FROM AccessLog acl JOIN acl.auditLog WHERE acl.auditLog.userId =:userId";
+		Query query = HibernateUtil.getSessionFactory().openSession().createQuery(queryString);
+		query.setString("userId", userId_fromAuditLog);
+		
+		List<AccessLog> accessLogs = HibernateUtil.performSimpleQuery(query);
+		return accessLogs;
+		
+		
+		
+	}
+	
+	
+	
+	
+}
